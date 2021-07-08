01b5320ddd8f64bb34b2a707681fdae1336d246e
Combatd <combatdstar@gmail.com>
2021-07-07 18:24:05 -0700

    Define Array::transpose

--- phase_1_arrays.js
+++ phase_1_arrays.js
@@ -27,5 +27,15 @@ Array.prototype.twoSum = function() {
 }
 
 Array.prototype.transpose = function() {
-    
+    const rotatedArr = [];
+    const newRows = []; // temporary container array
+
+    for(let i = 0; i < this.length; i++) {
+        for (let j = 0; j < this.length; j++) {
+            newRows.push(this[j][i]);
+        }
+        rotatedArr.push(newRows); // at the index of j, push all the i elements to take up where the j elements are in the original
+        newRows = []; // clear out the container array
+    }
+    return rotatedArr;
 }
