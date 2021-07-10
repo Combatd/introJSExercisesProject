82d66bbbc2d3f13552d6fbfdcd2d10d9668f4e4a
Combatd <combatdstar@gmail.com>
2021-07-09 17:27:13 -0700

    Define Array::myMap

--- phase_2_enumerable.js
+++ phase_2_enumerable.js
@@ -3,3 +3,9 @@ Array.prototype.myEach = function(callback) {
         callback(this[i]); // run the function on each element, don't return anything
     }
 }
+
+Array.prototype.myMap = function(callback) {
+    const newArray = [];
+    this.myEach(() => { newArray.push(callback(ele)) }); // use myEach and a closure
+    return newArray;
+}
