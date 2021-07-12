9a3d11e167a131e3fd27f7adac56e087202cf4d8
Combatd <combatdstar@gmail.com>
2021-07-11 20:53:06 -0700

    Define Array::myReduce

--- phase_2_enumerable.js
+++ phase_2_enumerable.js
@@ -9,3 +9,15 @@ Array.prototype.myMap = function(callback) {
     this.myEach(() => { newArray.push(callback(ele)) }); // use myEach and a closure
     return newArray;
 }
+
+Array.prototype.myReduce = function(callback, initialValue) {
+    // should be first element if initialValue is not provided
+    if (initialValue === undefined || null) {
+        initialValue = this[0];
+        this = this.slice(1); // get all elements after the initialValue
+    }
+
+    let result = initialValue; // our accumulator
+    arr.myEach((ele) => result = callback(result, ele)); // run the callback on the array starting at initialValue
+    return result; // return the accumulator
+}
